578b5018323c8de3bd688bf6e2ce714b4b828d2c
heyren66666 <1873714818@qq.com>
2019-11-06 10:28:22 +0800

    FileInputStream和FileReader举例

---
+++ src/Java240.java
@@ -0,0 +1,47 @@
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Java240 {
+    public static void main(String[] args)throws Exception{
+        FileInputStream fis=null;
+        try{
+            fis=new FileInputStream("testInput.txt");
+            int data=fis.read();
+            while (data!=-1){
+                System.out.print(Integer.toHexString(data));
+                data=fis.read();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (fis!=null){
+                try{
+                    fis.close();
+                }catch (IOException e){
+                }
+            }
+        }
+        System.out.println();
+        FileReader reader=null;
+        try{
+            reader=new FileReader("testInput.txt");
+            int character=reader.read();
+            while (character !=-1){
+                System.out.print((char)character);
+                character=reader.read();
+            }
+        }catch (IOException io){
+            System.out.println("Failed to read character data from File");
+            io.printStackTrace();
+        }finally {
+            if (reader!=null){
+                try {
+                    reader.close();
+                }catch (IOException e){
+
+                }
+            }
+        }
+    }
+}
