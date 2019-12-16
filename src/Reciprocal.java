d47ab7a7f7c06ba1e48416045f240894d5375911
heyren66666 <1873714818@qq.com>
2019-12-16 10:49:56 +0800

    单链表向右选择k个位置

--- src/Reciprocal.java
+++ src/Reciprocal.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 public class Reciprocal {
-
+/*    //链表中倒数第k个元素
     public static LNode ConstructList(){
         int i=1;
         LNode head=new LNode();
@@ -49,5 +49,59 @@ public class Reciprocal {
         result=FindLastK(head,3);
         if (result!=null)
             System.out.print("\n链表倒数第三个元素为："+result.data);
+    }*/
+
+
+    //单链表向右旋转k个位置
+    public static void RotateK(LNode head,int k){
+        if (head==null||head.next==null)
+            return;
+        LNode slow,fast,tmp;
+        slow=fast=head.next;
+        int i;
+        for (i=0;i<k&&fast!=null;++i){
+            fast=fast.next;
+        }
+        if (i<k)
+            return;
+        while (fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        tmp=slow;
+        slow=slow.next;
+        tmp.next=null;
+        fast.next=head.next;
+        head.next=slow;
+    }
+
+    public static LNode ConstructList(){
+        int i=1;
+        LNode head=new LNode();
+        head.next=null;
+        LNode tmp=null;
+        LNode cur=head;
+        for (;i<8;i++){
+            tmp=new LNode();
+            tmp.data=i;
+            tmp.next=null;
+            cur.next=tmp;
+            cur=tmp;
+        }
+        return head;
+    }
+
+    public static void PrintList(LNode head){
+        for (LNode cur=head.next;cur!=null;cur=cur.next)
+            System.out.print(cur.data+"");
+    }
+
+    public static void main(String[] args){
+        LNode head=ConstructList();
+        System.out.print("旋转前：");
+        PrintList(head);
+        RotateK(head,3);
+        System.out.print("\n旋转后：");
+        PrintList(head);
     }
 }
